6637f4af891dfa5fa9583516521df7f01c9843ad
JRSkates <skatesproduction@gmail.com>
2024-11-01 11:39:09 +0000

    inital project setup, sequelize conneciton, User model

--- server/src/db/config.js
+++ server/src/db/config.js
@@ -1,1 +1,15 @@
 // add your database connection here
+const path = require('path');
+const { DataTypes, Sequelize, Model } = require('sequelize');
+
+const db = new Sequelize ({
+    dialect: 'sqlite',
+    storage: path.join(__dirname, 'db.sqlite')
+})
+
+module.exports = {
+    db,
+    Sequelize,
+    DataTypes,
+    Model
+};
--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,1 +1,17 @@
 // create your User model here
+const {DataTypes, db, Model} = require('../db/config');
+
+class User extends Model {
+    static {
+        User.init({
+            username: DataTypes.STRING,
+    
+        }, {
+            sequelize: db,
+        })
+    }
+}
+
+module.exports = {
+    User,
+}
